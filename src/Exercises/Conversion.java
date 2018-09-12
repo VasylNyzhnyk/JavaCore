a2b935d4e6ca04458f73d524412f66c1e90e0ea4
Vasyl <Vasylnv19@gmail.com>
2018-09-12 13:23:53 +0300

    Vasyl

--- src/Exercises/Conversion.java
+++ src/Exercises/Conversion.java
@@ -15,6 +15,7 @@ public class Conversion {
         System.out.println("d та i " + d + " " + i);
 
         System.out.println("\nПеретворення типу double в тип int ");
+        System.out.println("Hello");
         b = (byte) d;
         System.out.println("d  та b " + d + " "+ b);
     }
