b00f6f45bfba0061bd8d72c38150f8e29a0b5313
Vasyl <Vasylnv19@gmail.com>
2018-09-17 15:53:39 +0300

    Vasyl

--- src/TicTacToe/Field.java
+++ src/TicTacToe/Field.java
@@ -24,9 +24,7 @@ import java.io.InputStreamReader;
     static void show() {
 
             System.out.println(" " + arr[0] + " | " + arr[1] + " | " + arr[2] + "  ");
-            System.out.println("--- --- ---");
             System.out.println(" " + arr[3] + " | " + arr[4] + " | " + arr[5] + "  ");
-            System.out.println("--- --- ---");
             System.out.println(" " + arr[6] + " | " + arr[7] + " | " + arr[8] + "  ");
             }
 
