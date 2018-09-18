5a100dab3a33ed8eb43db1decbae06a90d262150
Vasyl <Vasylnv19@gmail.com>
2018-09-18 22:55:26 +0300

    Vasyl

--- src/TicTacToe/GameLogic.java
+++ src/TicTacToe/GameLogic.java
@@ -54,33 +54,7 @@ public class GameLogic {
         }
     }
 
-    /*static void checkWinner(){
-        for ( int n = 0; n < arr.length - 3; n = n + 3){
-            if (arr[n] == arr[n+1] & arr[n] == arr[n+2] & arr[n] != 0){
-                System.out.println("Переміг гравець " +(j%2+1));
 
-            }
-        }
-        for ( int n = 0; n < 3; n++){
-            if (arr[n] == arr[n+3] & arr[n] == arr[n+6] & arr[n] != 0){
-                System.out.println("Переміг гравець " +(j%2+1));
-
-            }
-        }
-        int n = 0;
-        if (arr[n] == arr[n+4] & arr[n] == arr[n+8] & arr[n] != 0){
-            System.out.println("Переміг гравець " +(j%2+1));
-
-        }
-        if (arr[n+2] == arr[n+4] & arr[n+2] == arr[n+6] & arr[n+2] != 0){
-            System.out.println("Переміг гравець " +(j%2+1));
-
-        }
-        //Умова нічиї;
-        if ( j == 8){
-            System.out.println("Нічия");
-
-        }*/
 }
 
 
