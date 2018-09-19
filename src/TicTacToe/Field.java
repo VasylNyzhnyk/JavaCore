b06bea11c20d853cd75c31d1875847ae912f91f6
Vasyl <Vasylnv19@gmail.com>
2018-09-19 15:55:49 +0300

    Vasyl

--- src/TicTacToe/Field.java
+++ src/TicTacToe/Field.java
@@ -3,18 +3,9 @@ package TicTacToe;
  class Field {
 
      static char[] arr = new char[9];
-
-     static void getUserMessage() {
-        System.out.println(" 0 | 1 | 2 ");
-        System.out.println("--- --- ---");
-        System.out.println(" 3 | 4 | 5 ");
-        System.out.println("--- --- ---");
-        System.out.println(" 6 | 7 | 8 ");
-
-    }
-
-
-    static void show() {
+     static char USER_SYMBOL = 'o';
+     static char PC_SYMBOL = 'x';
+     static void show() {
 
         System.out.println(" "+arr[0]+" | "+arr[1]+" | "+arr[2]+"  ");
         System.out.println("--- --- ---");
--- src/TicTacToe/GameLogic.java
+++ src/TicTacToe/GameLogic.java
@@ -4,9 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-import static TicTacToe.Field.arr;
-import static TicTacToe.Field.getUserMessage;
-import static TicTacToe.Field.show;
+import static TicTacToe.Field.*;
 
 public class GameLogic {
 
@@ -18,22 +16,29 @@ public class GameLogic {
                 System.out.println("Ходить O");
                 int number = getNumber();
                 arr[number] = (char) number;
-                arr[number] = 'o';
+                arr[number] = USER_SYMBOL;
+
                 show();
             } else {
                 System.out.println("Хід компютера X ");
                 randomNumder();
                 show();
+
             }
         }
-
-
     }
 
     static void randomNumder() {
-        int number = (char) (Math.random() * 9);
-        arr[number] = (char) number;
-        arr[number] = 'x';
+        while (true) {
+            try {
+                int n = (int) (Math.random() * 9);
+                if (arr[n] == 0) {
+                    arr[n] = PC_SYMBOL;
+                    return;
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+            }
+        }
     }
 
     static int getNumber() {
@@ -44,9 +49,9 @@ public class GameLogic {
                 if (n >= 0 && n <= arr.length && arr[n] == 0) {
                     return n;
                 }
-                System.out.println("Виберіть відьну комірку і введіть її номер");
+                System.out.println("Виберіть вільну комірку і введіть її номер");
             } catch (NumberFormatException e) {
-                System.out.println("Виберіть відьну комірку і введіть її номер");
+                System.out.println("Виберіть вільну комірку і введіть її номер");
             } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("Будь ласка, введіть номер");
             } catch (IOException e) {
@@ -54,6 +59,28 @@ public class GameLogic {
         }
     }
 
+    static void getUserMessage() {
+        System.out.println(" 0 | 1 | 2 ");
+        System.out.println("--- --- ---");
+        System.out.println(" 3 | 4 | 5 ");
+        System.out.println("--- --- ---");
+        System.out.println(" 6 | 7 | 8 ");
+
+    }
+
+    static boolean checkWinner(char n) {
+        int row = n - n % 3;
+        if (arr[row] == arr[row + 1] && arr[row] == arr[row + 2]) return true;
+        int column = n % 3;
+        if (arr[column] == arr[column + 3])
+            if (arr[column] == arr[column + 6]) return true;
+        if (n % 2 != 0) return false;
+        if (n % 4 == 0) {
+            if (arr[0] == arr[4] && arr[0] == arr[8]) return true;
+            if (n != 4) return false;
+        }
+        return arr[2] == arr[4] && arr[2] == arr[6];
+    }
 
 }
 
